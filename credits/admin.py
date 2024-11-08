9893216971750737626bc9e67a352e7fe94d22dc
Edwin Andres Duarte Rodriguez <edanduarte1@hotmail.com>
2024-11-07 23:52:31 -0500

    build: change in the structure

--- credits/admin.py
+++ credits/admin.py
@@ -2,11 +2,3 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Client)
-admin.site.register(Employ)
-admin.site.register(Admin)
-admin.site.register(Product_type)
-admin.site.register(Product)
-admin.site.register(Credit)
-admin.site.register(Payment)
--- credits/models.py
+++ credits/models.py
@@ -1,61 +1,53 @@
 from django.db import models
 
+from clients.models import Client
+from easycredit_app import settings
+from products.models import Product
+
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=255)
-
-class Client(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Employ(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Admin(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Product_type(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    def __str__(self):
-        return self.name
-
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField() # quantity available
-    product_type = models.ForeignKey(Product_type, on_delete=models.CASCADE)
-    def __str__(self):
-        return f"{self.name} - {self.product_type.name}"
-
 
 class Credit(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True) # Date the credit was created
-    interest = models.DecimalField(max_digits=5, decimal_places=2)
-    time_limit = models.IntegerField()  # Credit term in months
-
-    STATUS_CHOICES = [
-        ('STR', 'Start'),
-        ('ACT', 'Active'),
-        ('CMP', 'Completed'),
-    ]
-    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='STR')
+    STATUSES = {
+        "pending": "Pending",
+        "approved": "Approved",
+        "rejected": "Rejected",
+        "active": "Active",
+        "completed": "Completed",
+        "past_due": "Past Due"
+    }
+    status = models.CharField(max_length=55, choices=STATUSES)
+    client = models.ForeignKey(Client, on_delete=models.RESTRICT)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT)
+    installments = models.SmallIntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.RESTRICT)
+    # interest_rate = models.ForeignKey(InterestRate, default=0.0, on_delete=models.RESTRICT)
+    start_date = models.DateField(null=False)
+    estimated_end_date = models.DateField
+    application_date = models.DateField(null=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
     def __str__(self):
         return f"Client: {self.client} - Product: {self.product}"
     
 class Payment(models.Model):
-    credit = models.ForeignKey(Credit, on_delete=models.CASCADE)
-    value = models.DecimalField(max_digits=10, decimal_places=2)
+    
 
-    STATUS_CHOICES = [
-        ('PEN', 'Pending'),
-        ('CMP', 'Completed'),
-        ('DLY', 'Delayed'),
-    ]
-    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='PEN')
+    STATUSES = {
+        'pending': 'Pending',
+        'completed': 'Completed',
+        'failed': 'Failed'
+    }
+
+    TYPES = {
+        'regular': 'Regular Payment',
+        'advance': 'Advance Payment'
+    }
+    credit = models.ForeignKey(Credit, on_delete=models.RESTRICT)
+    date = models.DateTimeField(null=False)
+    type = models.CharField(max_length=55, choices=TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    status = models.CharField(max_length=55, choices=STATUSES)
+    overdue = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.credit} - {self.date} - {self.amount} - {self.status}'
